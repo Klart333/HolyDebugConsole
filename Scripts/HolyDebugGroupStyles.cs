577de499ccd80fb7f8bf4883bd97cfebd26311b4
John Roddis <Roddis.John1@gmail.com>
2026-06-17 09:37:12 +0200

    Fixed block UI

--- Scripts/HolyDebugGroupStyles.cs
+++ Scripts/HolyDebugGroupStyles.cs
@@ -9,23 +9,7 @@ public static partial class HolyDebugGroupStyles {
     
     public const string Uncategorized = "Uncategorized";
     [DebugCommandGroup(Uncategorized)] public static readonly DebugGroupStyle UncategorizedStyle 
-        = new DebugGroupStyle("❔ Uncategorized", Color.white);
-    
-    public const string SaveSytem = "Save System 💾";
-    [DebugCommandGroup(SaveSytem)] public static readonly DebugGroupStyle SaveSystemStyle 
-        = new DebugGroupStyle("Save System", new Color(0.39f, 0.35f, 0.58f));
-    
-    public const string Enemy = "Enemy 👾";
-    [DebugCommandGroup(Enemy)] public static readonly DebugGroupStyle EnemyStyle 
-        = new DebugGroupStyle("Enemy", new Color(0.8f, 0.2f, 0.2f));
-    
-    public const string Tower = "Tower 🗼";
-    [DebugCommandGroup(Tower)] public static readonly DebugGroupStyle TowerStyle 
-        = new DebugGroupStyle("Tower", new Color(0.2f, 0.6f, 0.8f));
-
-    public const string Loot = "Loot 💰";
-    [DebugCommandGroup(Loot)] public static readonly DebugGroupStyle LootStyle 
-        = new DebugGroupStyle("Loot", new Color(1f, 0.84f, 0f));
+        = new DebugGroupStyle("Uncategorized ❔", Color.white);
 }
 
 public readonly struct DebugGroupStyle : IEquatable<DebugGroupStyle>
